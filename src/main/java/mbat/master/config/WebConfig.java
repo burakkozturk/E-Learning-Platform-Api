5f9a9447d34cb6921d9017f5c969fa55f98f33ba
burak <devx.burak@gmail.com>
2024-05-28 02:13:52 +0300

    Added CORS configuration

--- src/main/java/mbat/master/config/WebConfig.java
+++ src/main/java/mbat/master/config/WebConfig.java
@@ -11,10 +11,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://mbat4seniors.herokuapp.com",
-                        "https://https://mbat-platform.netlify.app","https://mighty-brook-61779.herokuapp.com")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
